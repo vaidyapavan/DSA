e1b1c971c5a1bde78e811bb1dbeab2752bf6d186
Pavan vaidya <115892894+vaidyapavan@users.noreply.github.com>
2024-12-22 22:44:51 +0530

    Update triplet_sum.java

--- triplet_sum.java
+++ triplet_sum.java
@@ -1,38 +1,31 @@
 package Array;
 
+class Triplet {
+    int arr[] = {5, 7, 8, 2, 3, 4, 5, 6, 7, 1, 2, 56, 2};
+    int n = arr.length; // Corrected length calculation
 
-class triplte
-{
-	int arr[]= {5,7,8,2,3,4,5,6,7,1,2,56,2};
-	int n= arr.length-1;
-	void sum()
-	{
-		int target_sum=10;
-		int count=0;
-		for(int i=0;i<n;i++)
-		{
-			for(int j=i+1;j<n;j++)
-			{
-				for(int k=j+1;k<n;k++)
-				{
-					if(arr[i]+arr[j]+arr[k]==target_sum)
-					{
-						count++;
-						
-                         System.out.println(arr[i] + ", " + arr[j] + ", " + arr[k]);
-					}
-				}
-			}
-		}
-		System.out.println("no og trilets are: "+count);
-	}
+    void sum() {
+        int target_sum = 10;
+        int count = 0;
+
+        // Iterate over all possible triplets
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == target_sum) {
+                        count++;
+                        System.out.println(arr[i] + ", " + arr[j] + ", " + arr[k]);
+                    }
+                }
+            }
+        }
+        System.out.println("Number of triplets: " + count);
+    }
 }
-public class triplet_sum {
 
-	public static void main(String[] args) {
-		triplte obj = new triplte();
-		obj.sum();
-		
-	}
-
+public class TripletSum {
+    public static void main(String[] args) {
+        Triplet obj = new Triplet();
+        obj.sum();
+    }
 }
